1ef103577f1e0eb2c0fd554b69bb6b6c2a6a15d4
iXavy <ict@ixavy.com>
2022-10-04 12:07:23 +0200

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by iXavy on 4/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
