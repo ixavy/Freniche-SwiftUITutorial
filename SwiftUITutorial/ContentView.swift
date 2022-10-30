7261d959a0a8de0ee092844e999fdefc4c623953
iXavy <ict@ixavy.com>
2022-10-30 18:44:34 +0100

    another commit

--- SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/ContentView.swift
@@ -7,18 +7,59 @@
 
 import SwiftUI
 
+let picachu = "Picachu"
+let charmander = "Charmander"
+let bulbasaur = "Bulbasaur"
+
+let pockemons = [
+    picachu,
+    charmander,
+    bulbasaur]
+
 struct ContentView: View {
+ 
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
+      
             Text("Hello, cabrones!")
                 .font(.title)
                 .foregroundColor(Color.blue)
+           
+                Text("COMO ESTAIS")
+                    .font(.footnote)
+                    .padding(0.0)
+            HStack {
+                Button("Boton", action: accion)
+                    .padding()
+                PokemonButton(title: "👈") {
+                    print("Izquierda")
+                }
+                
+                PokemonButton(title: "👉") {
+                    print("Derecha")
+                }
+            }
+            
         }
-        .padding()
     }
+    
+    func accion() {
+        print ("Has presionado boton")
+    }
+}
+
+struct PokemonButton: View {
+    let title: String
+    let action: () -> Void
+    
+    var body: some View {
+        Button(_: title, action: action)
+            .padding(40.0)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -26,3 +67,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
